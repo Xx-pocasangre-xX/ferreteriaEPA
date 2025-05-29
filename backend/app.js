d6320e7d7baba2d974d9937b1311db3323b0396b
Ricardo <danielpocasangre2006@gmail.com>
2025-05-29 10:47:09 -0600

    despues de un temblor

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: "ferreteria-epa-phi.vercel.app",
+        origin: "https://ferreteria-epa-phi.vercel.app",
         credentials: true
     })
 );
